23207c85ce299742e4f852d80e5411f4634403fe
Evelyn Luu <eyluu@uci.edu>
2015-06-08 13:28:59 -0700

    fin

--- index.php
+++ index.php
@@ -23,35 +23,33 @@
 				<form id="timerForm">
 					<!-- Work Timer Dropdown -->
 					<tr>
-						<td colspan="2"><h2>Set Work Timer:</h2></td>
+						<td colspan="3"><h2>Set Work Timer:</h2></td>
 						
 						<td colspan="2">
 							<select name="workDuration" class="timerSelect">
-								<option value="1">1</option> <!-- For demo-ing purposes -->
 								<option value="15">15</option>
 								<option value="30">30</option>
 							</select>
 						</td>
 						
-						<td><p>minutes</p></td>
+						<td colspan="2"><p>minutes</p></td>
 					</tr>
 					<!-- Break Timer Dropdown -->
 					<tr>
-						<td colspan="2"><h2>Set Break Timer:</h2></td>
+						<td colspan="3"><h2>Set Break Timer:</h2></td>
 						
 						<td colspan="2">
 							<select name="breakDuration" class="timerSelect">
-								<option value="1">1</option> <!-- For demo-ing purposes -->
 								<option value="5">5</option>
 								<option value="10">10</option>
 							</select>
 						</td>
 						
-						<td><p>minutes</p></td>
+						<td colspan="2"><p>minutes</p></td>
 					</tr>
 					<!-- Start Button -->
 					<tr>
-						<td colspan="6" style="text-align:center; padding-top: 0.5em;">
+						<td colspan="7" style="text-align:center; padding-top: 0.5em;">
 							<button id="workTimerBtn" onclick="startWorkTimer()">Start</button>
 						</td>
 					</tr>
